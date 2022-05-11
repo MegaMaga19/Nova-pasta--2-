962823ad08ed8284d963c3b31423e685c3565255
Matheus <matheusop806@gmail.com>
2022-05-11 20:22:52 -0300

    maga

---
+++ src/components/Cabecalho.tsx
@@ -0,0 +1,9 @@
+const Cabecalho = function() {
+    return (
+        <header>
+            <span>App Itens</span>
+        </header>
+    );
+};
+
+export default Cabecalho;
---
+++ src/components/Rodape.tsx
@@ -0,0 +1,10 @@
+const Rodape = function() {
+    return (
+        <footer>
+            <p>Desenvolvido por matheus </p>
+        
+        </footer>
+    );
+};
+ 
+export default Rodape;
